1ed25a5131154e737a26b1346ede6afa09165324
Kaishi_Iwabuchi <repayment.inc@gmail.com>
2023-01-09 17:06:31 +0900

    #1,Email,Password登録機能

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,97 @@
-fn main() {
-    println!("Hello, world!");
-    println!("Test");
+// 参考サイト https://medium.com/intelliconnect-engineering/using-axum-framework-to-create-rest-api-part-1-7d434d2c5de4
+
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    response::IntoResponse,
+    Json, Router,
+};
+
+use sqlx::postgres::PgPoolOptions;
+use tower_http::cors::{Any, CorsLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use serde::{Deserialize, Serialize};
+
+mod controllers;
+mod error;
+mod models;
+
+// コンパイル時、環境変数設定が必要
+// DATABASE_URL=postgresql://ユーザ名:パスワード@localhost:5432/mydb cargo run
+
+#[tokio::main]
+async fn main() {
+    let durl = std::env::var("DATABASE_URL").expect("set DATABASE_URL env variable");
+
+    // initialize tracing
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "axum_api=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let cors = CorsLayer::new().allow_origin(Any);
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&durl)
+        .await
+        .expect("unable to connect to database");
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        .route("/register", post({
+            move |body| controllers::auth::register(body, axum::extract::State(pool))
+            }),
+        )
+        // `POST /users` goes to `create_user`
+        .route("/users", post(create_user))
+        .layer(cors);
+        // .with_state(pool);
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("failed to start server");
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> impl IntoResponse {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
