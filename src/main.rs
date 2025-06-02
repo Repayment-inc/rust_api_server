d3ee7c5f4b241abe3a61a8d1012534b533f7d588
Kaishi_Iwabuchi <repayment.inc@gmail.com>
2025-06-03 06:12:35 +0900

    コミット忘れ。内容不明

--- src/main.rs
+++ src/main.rs
@@ -61,7 +61,7 @@ async fn main() {
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
     let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listening on {}", addr);
+    tracing::debug!("Server started, listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
